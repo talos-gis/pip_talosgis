4859694d881ef4eec62f4841f43da5d8f476595a
Idan Miara <idan@miara.com>
2020-09-07 18:42:04 +0300

    .

--- download_requirements.bat
+++ download_requirements.bat
@@ -1,4 +1,6 @@
 @echo Are you sure you want to download all the requirements? 
 @echo Press Ctrl+C to abort
+pause
 set wheels=%~dp0\wheels\
-for %%r in (requirements*.txt) do python -m pip download -r %%r -d %wheels%
+if %1x==x (set requirements=requirements*.txt) else (set requirements=%*)
+for %%r in (%requirements%) do python -m pip download -r %%r -d %wheels%
--- install_requirements.bat
+++ install_requirements.bat
@@ -2,4 +2,5 @@
 @echo Press Ctrl+C to abort
 pause
 set wheels=%~dp0\wheels\
-for %%r in (requirements.txt) do python -m pip install --no-index --find-links %wheels% -r %%r
+if %1x==x (set requirements=requirements.txt) else (set requirements=%*)
+for %%r in (%requirements%) do python -m pip install --no-index --find-links %wheels% -r %%r
